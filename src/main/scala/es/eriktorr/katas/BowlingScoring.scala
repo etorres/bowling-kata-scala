8b70e8ec40867a2b6168718c07417b597fecd253
Erik Torres <erik.torres@lifullconnect.com>
2019-11-17 22:36:34 +0100

    Save WiP

--- src/main/scala/es/eriktorr/katas/BowlingScoring.scala
+++ src/main/scala/es/eriktorr/katas/BowlingScoring.scala
@@ -1,7 +1,24 @@
 package es.eriktorr.katas
 
+import es.eriktorr.katas.FrameSplitter.framesFrom
+
+import scala.annotation.tailrec
+
 object BowlingScoring {
 
-  def scoreGame(value: String): Int = 300
+  def scoreGame(game: String): Int = {
+    val frames = framesFrom(game).to(List)
+    score(frames, 0)
+  }
+
+  @tailrec
+  def score(frames: List[Array[String]], scoreAccumulator: Int): Int = frames match {
+    case Nil => scoreAccumulator
+    case first :: allExceptFirst => score(allExceptFirst, scoreAccumulator + score(first))
+  }
+
+  val score: Array[String] => Int = (frame: Array[String]) => {
+    1
+  }
 
 }
