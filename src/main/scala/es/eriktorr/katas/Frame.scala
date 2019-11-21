f8324690666720060233167d98666f8ac6a2875f
Erik Torres <erik.torres@lifullconnect.com>
2019-11-21 09:29:52 +0100

    Minor improvement to Frame class

--- src/main/scala/es/eriktorr/katas/Frame.scala
+++ src/main/scala/es/eriktorr/katas/Frame.scala
@@ -1,17 +1,19 @@
 package es.eriktorr.katas
 
 object FrameBuilder {
-  private case class Regular(balls: Seq[Int]) extends Frame {
-    override def scoringBalls: Int = 2
+  private case class Regular(firstBall: Int, secondBall: Int) extends Frame {
+    override val balls: Seq[Int] = Seq(firstBall, secondBall)
+    override val scoringBalls: Int = 2
   }
 
-  private case class Spare(balls: Seq[Int]) extends Frame {
-    override def scoringBalls: Int = 3
+  private case class Spare(firstBall: Int) extends Frame {
+    override val balls: Seq[Int] = Seq(firstBall, 10 - firstBall)
+    override val scoringBalls: Int = 3
   }
 
   private case object Strike extends Frame {
-    override def balls: Seq[Int] = Seq(10)
-    override def scoringBalls: Int = 3
+    override val balls: Seq[Int] = Seq(10)
+    override val scoringBalls: Int = 3
   }
 
   def apply(frameScore: String): Frame = frameFrom(frameScore)
@@ -25,13 +27,11 @@ object FrameBuilder {
 
     val frame = frameScore match {
       case "X" => Strike
-      case "--" => Regular(Seq(0, 0))
-      case s"$a-" => Regular(Seq(a.toInt, 0))
-      case s"-$b" => Regular(Seq(0, b.toInt))
-      case s"$a/" =>
-        val firstBall = a.toInt
-        Spare(Seq(firstBall, 10 - firstBall))
-      case r"(\d)$a(\d)$b" => Regular(Seq(a.toInt, b.toInt))
+      case "--" => Regular(0, 0)
+      case s"$a-" => Regular(a.toInt, 0)
+      case s"-$b" => Regular(0, b.toInt)
+      case s"$a/" => Spare(a.toInt)
+      case r"(\d)$a(\d)$b" => Regular(a.toInt, b.toInt)
       case _ => throw new IllegalArgumentException(s"failed to parse frame score $frameScore")
     }
     frame
@@ -39,6 +39,6 @@ object FrameBuilder {
 }
 
 trait Frame {
-  def balls: Seq[Int]
-  def scoringBalls: Int
+  val balls: Seq[Int]
+  val scoringBalls: Int
 }
